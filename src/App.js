a089824d3a9fee2773259f77614fa8fba4d7519a
GiRang Choi <choigirang@kakao.com>
2023-02-27 19:47:10 +0900

    feat: Navigation과 Banner를 포함한 App 컴포넌트 작성 /ch.01
    styled-components 적용

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px; // Nav 보다 아래에 위치
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Nav />
+      <Banner />
+    </Container>
+  );
+}
+
+export default App;
